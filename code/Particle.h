6428f6c0a42f3e7f1b1f1b65a7329ea5650cd59f
MarkMudik <MarkMudik123@gmail.com>
2025-12-11 13:19:30 -0800

    Added ifndef around M_PI definition for no redefine error

--- code/Particle.h
+++ code/Particle.h
@@ -2,7 +2,10 @@
 #include "Matrices.h"
 #include <SFML/Graphics.hpp>
 
+#ifndef M_PI
 #define M_PI 3.1415926535897932384626433
+#endif
+
 const float G = 1000;      //Gravity
 const float TTL = 5.0;  //Time To Live
 const float SCALE = 0.999;
